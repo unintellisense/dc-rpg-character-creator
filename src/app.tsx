7499d99614dd09ddbf27a836afe601488fd17868
unintellisense <weldonconsulting@gmail.com>
2020-08-26 00:44:37 -0700

    something

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,25 @@
 import * as React from 'react'
+import { TextField, Grid } from '@material-ui/core';
 
 export default function App() {
-    return <div>Hmmm</div>
+    return <form>
+
+        <Grid container>
+            <Grid container >
+                <Grid item xs={4}><Grid container justify="center" alignItems="center"><TextField label="Dex" type="number" ></TextField></Grid></Grid>
+                <Grid item xs={4}><Grid container justify="center" alignItems="center"><TextField label="Str" type="number" ></TextField></Grid></Grid>
+                <Grid item xs={4}><Grid container justify="center" alignItems="center"><TextField label="Body" type="number" ></TextField></Grid></Grid>
+            </Grid>
+            <Grid container >
+                <Grid item xs={4}><Grid container justify="center" alignItems="center"><TextField label="Int" type="number" ></TextField></Grid></Grid>
+                <Grid item xs={4}><Grid container justify="center" alignItems="center"><TextField label="Will" type="number" ></TextField></Grid></Grid>
+                <Grid item xs={4}><Grid container justify="center" alignItems="center"><TextField label="Mind" type="number" ></TextField></Grid></Grid>
+            </Grid>
+            <Grid container >
+                <Grid item xs={4}><Grid container justify="center" alignItems="center"><TextField label="Infl" type="number" ></TextField></Grid></Grid>
+                <Grid item xs={4}><Grid container justify="center" alignItems="center"><TextField label="Aura" type="number" ></TextField></Grid></Grid>
+                <Grid item xs={4}><Grid container justify="center" alignItems="center"><TextField label="Spirit" type="number" ></TextField></Grid></Grid>
+            </Grid>
+        </Grid>
+    </form>
 }
